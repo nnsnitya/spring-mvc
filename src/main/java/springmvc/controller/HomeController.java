b17784af6ed06edeae447b609c22f56bc6448788
Nityanand Singh <nns.nitya@gmail.com>
2024-01-26 23:10:50 +0530

    sending attr to view using model obj

--- src/main/java/springmvc/controller/HomeController.java
+++ src/main/java/springmvc/controller/HomeController.java
@@ -1,6 +1,10 @@
 package springmvc.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -8,8 +12,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class HomeController {
 	
 	@GetMapping("/home")
-	public String home() {
+	public String home(Model model) {
 		System.out.println("this is home url");
+		model.addAttribute("name", "Nityanand Singh");
+		model.addAttribute("id",12345);
+		
+		List<String> friends = new ArrayList<String>();
+		friends.add("Pankaj");
+		friends.add("Virendra");
+		friends.add("Premshanker");
+		model.addAttribute("f", friends);
+		
 		return "index";
 	}
 	@RequestMapping("/about")
--- src/main/webapp/pages/index.jsp
+++ src/main/webapp/pages/index.jsp
@@ -1,3 +1,4 @@
+<%@page import="java.util.List"%>
 <%@ page language="java" contentType="text/html; charset=ISO-8859-1"
     pageEncoding="ISO-8859-1"%>
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
@@ -10,5 +11,23 @@
 	<h1>This is home page</h1>
 	<h1>Called by home controller</h1>
 	<h1>url /home</h1>
+	<%
+		String name=(String)request.getAttribute("name");
+		Integer id = (Integer)request.getAttribute("id");
+		List<String> friends = (List<String>)request.getAttribute("f");
+	%>
+	<h1>Name is <%=name%></h1>
+	<h1>Id is <%=id%></h1>
+	<%-- <%
+		for(String s: friends){
+			out.println(s);
+		}
+	%> --%>
+	<%
+		for(String s: friends) {
+	%>
+	<h1><%=s %></h1>
+	<%	}
+	%>
 </body>
 </html>
