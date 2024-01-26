f9d849f47a35783e5cfc09a37271b7654b0747fa
Nityanand Singh <nns.nitya@gmail.com>
2024-01-26 22:53:12 +0530

    added about page

--- src/main/java/springmvc/controller/HomeController.java
+++ src/main/java/springmvc/controller/HomeController.java
@@ -2,6 +2,7 @@ package springmvc.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
@@ -10,6 +11,11 @@ public class HomeController {
 	public String home() {
 		System.out.println("this is home url");
 		return "index";
+	}
+	@RequestMapping("/about")
+	public String about() {
+		System.out.println("this is about url");
+		return "about";
 	}
 	
 }
