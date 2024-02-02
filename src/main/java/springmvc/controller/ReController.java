3d72e83c6304f71db197a69f2af4bff6ae900799
Nityanand Singh <nns.nitya@gmail.com>
2024-02-02 22:00:32 +0530

    RedirectView to redirect
    0. HttpServletResponse
    
    1. Redirect Prefix
    2. RedirectView

--- src/main/java/springmvc/controller/ReController.java
+++ src/main/java/springmvc/controller/ReController.java
@@ -2,15 +2,23 @@ package springmvc.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.view.RedirectView;
 
 @Controller
 public class ReController {
 	
 	@RequestMapping("/one")
+	public RedirectView one() {
+		System.out.println("This is one handler");
+		RedirectView redirectView = new RedirectView();
+		redirectView.setUrl("https://www.google.com");
+		return redirectView;
+	}
+	/*@RequestMapping("/one")
 	public String one() {
 		System.out.println("This is one handler");
 		return "redirect:/enjoy";
-	}
+	}*/
 	
 	@RequestMapping("/enjoy")
 	public String two() {
